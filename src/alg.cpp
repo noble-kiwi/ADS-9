a8e98dbcfaca39edc60fc822b8b245f033a20e38
noble_kiwi <iwkr1208@gmail.com>
2026-05-29 16:43:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,5 +3,21 @@
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
+#include  <vector>
 #include  "tree.h"
 
+std::vector<std::vector<char>> getAllPerms(const PMTree& tree) {
+    return tree.getPermutations();
+}
+
+std::vector<char> getPerm1(const PMTree& tree, int num) {
+    std::vector<std::vector<char>> perms = tree.getPermutations();
+    if (num < 1 || num > static_cast<int>(perms.size())) {
+        return std::vector<char>();
+    }
+    return perms[num - 1];
+}
+
+std::vector<char> getPerm2(const PMTree& tree, int num) {
+    return tree.navigate(num);
+}
